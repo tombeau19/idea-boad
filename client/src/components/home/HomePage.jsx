1c0d3b8bc9b7f40530ea3619265f3be5035fe3f2
tombeau19 <thomas.beauregard@gmail.com>
2017-10-12 14:29:10 -0400

    react router set up

---
+++ client/src/components/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                hello im homepage
+            </div>
+        );
+    }
+}
+
+export default HomePage;
---
+++ client/src/components/idea/IdeaPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class IdeaPage extends Component {
+    render() {
+        return (
+            <div>
+                im idea page
+            </div>
+        );
+    }
+}
+
+export default IdeaPage;
